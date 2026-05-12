085124469d7cbe776c3cdd5b821b8d2a6b394905
Marl0023 <marlotay2312@gmail.com>
2026-05-12 12:17:05 -0500

    primer commit

---
+++ Arbol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbolb
+{
+    internal class Arbol
+    {
+        public Nodo Raiz;
+
+        public void Insertar(int v)
+        {
+            Raiz = InsertarRecursivo(Raiz, v);
+        }
+        private Nodo InsertarRecursivo(Nodo raiz, int valor)
+        {
+            if (raiz == null) return new Nodo(valor);
+
+            if (valor < raiz.valor)
+                raiz.izq = InsertarRecursivo(raiz.izq, valor);
+            else
+                raiz.der = InsertarRecursivo(raiz.der, valor);
+
+            return raiz;
+        }
+        public void RecorrerEnOrden(Nodo raiz)
+        {
+            if (raiz != null)
+            {
+                RecorrerEnOrden(raiz.izq);
+                Console.Write(raiz.valor + " ");
+                RecorrerEnOrden(raiz.der);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbolb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arbol a = new Arbol();
+            a.Insertar(50);
+            a.Insertar(30);
+            a.Insertar(70);
+            a.Insertar(20);
+            a.Insertar(40);
+            a.Insertar(60);
+            a.Insertar(80);
+
+            Console.WriteLine("Recorrido en orden:");
+            a.RecorrerEnOrden(a.Raiz);
+        }
+    }
+}
